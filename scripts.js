0a45e282a259916ef0426d80018e26a7d8887500
Rob <klassen.rob@gmail.com>
2021-09-18 21:33:11 -0400

    initial commit, basice framework and bootstap file structure

---
+++ scripts.js
@@ -0,0 +1,165 @@
+// http://api.airvisual.com/v2/cities?state={{STATE_NAME}}&country={{COUNTRY_NAME}}&key={{YOUR_API_KEY}}
+//http://api.airvisual.com/v2/nearest_city?key={{YOUR_API_KEY}}
+
+//http://api.airvisual.com/v2/city_ranking?key={{YOUR_API_KEY}}
+//http://api.airvisual.com/v2/nearest_city?lat={{LATITUDE}}&lon={{LONGITUDE}}&key={{YOUR_API_KEY}}
+//43.19609391085522, -79.22207059039704
+
+
+
+
+
+// 
+
+
+
+
+
+const app = {};
+
+// "http://api.airvisual.com/v2/city"
+// http://api.airvisual.com/v2/nearest_city
+
+
+// key: testApp.apiKey,
+// country: 'Canada',
+// state: 'Ontario',
+// city: 'Toronto'
+
+
+app.apiUrl = "http://api.airvisual.com/v2/";
+app.apiKey = "a2793ab9-eff7-4732-9994-6e2320b1f247";
+// testApp.getPhotos = () => {
+//     const url = new URL(testApp.apiUrl);
+//     url.search = new URLSearchParams({
+//       key: testApp.apiKey,
+//       country: 'Canada',
+//       state: 'Ontario',
+//       city: 'Toronto'
+//     })
+
+app.getApiData = function(){
+    const url = new URL(app.apiUrl);
+    url.search = new URLSearchParams({
+        country: 'Canada'
+        // lat: 43.196,
+        // lon: -79.222,
+        // key: app.apiKey
+    });
+
+
+
+    fetch(url)
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    }).then(function(jsonResponse){
+        console.log(jsonResponse);
+        console.log(jsonResponse.data.current.pollution.aqicn);
+    });
+}
+
+app.init = function(){
+    app.getApiData();
+}
+
+app.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const galleryApp = {}
+
+// galleryApp.apiUrl = "https://api.unsplash.com/photos";
+// galleryApp.apiKey = "24hJVNhuvMBZwTqryXAtdpqiPpvgcpO9tR4b6ZKelUo";
+// // galleryApp.apiSecretKey = "v_s67e8KHCRfNAg28SknUvoxFOxmI6TZVp9YsIn9Fd0";
+
+// galleryApp.getPhotos = function(){
+//     const url = new URL(galleryApp.apiUrl);
+//     url.search = new URLSearchParams({
+//         client_id: galleryApp.apiKey
+//         // client_idSecret: galleryApp.apiSecretKey,
+//     });
+
+//     fetch(url)
+//     .then(function(response){
+
+//         return response.json();
+//     }).then(function(jsonResponse){
+//         console.log(jsonResponse);
+//     });
+// }
+
+
+
+
+// galleryApp.init = function(){
+//     galleryApp.getPhotos();
+// }
+
+// galleryApp.init();
+
+
+
+
+
+
+
+
+
+
+// fetch('https://www.septastats.com/api/current/system/latest')
+//     .then(function(response){
+//         return response.json();
+//     }).then(function(jsonData){
+
+//         // const route = jsonData.data[0].id;
+//         // const startStation = jsonData.data[0].source;
+//         // const endStation = jsonData.data[0].dest;
+//         // const minsLate = jsonData.data[0].late;
+
+//         // const paragraphEl = document.querySelector('p.nextDeparture');
+
+//         // paragraphEl.innerHTML = `The ${route} train from ${startStation} to ${endStation} is running ${minsLate} minutes late.`
+
+
+//         console.log(jsonData.data);
+        
+//         let paragraphEl = document.querySelector('p.nextDeparture');
+
+//         for (i=0; i<jsonData.data.length; i++){
+
+//             let trainDest2 = jsonData.data[i].dest;
+//             let trainLate2 = jsonData.data[i].late;
+
+//             console.log(`The train heading to ${trainDest2} and is ${trainLate2} minutes late`);
+//         }
+//     });
+
